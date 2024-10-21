07ff5ff498e4e8df0b4ca6d0f4866674b2a15296
RodNalCY <crodnal07@gmail.com>
2024-10-20 19:01:19 -0500

    Finish | Primera Etapa de Aplicacion

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
 import 'package:miauapp_flutter_app/home/widgets/actions_widget.dart';
-import 'package:miauapp_flutter_app/login/screens/login_screen.dart';
 import 'package:miauapp_flutter_app/menu/drawer/side_menu.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -21,19 +18,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       drawer: SideMenu(),
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Home",
           style: TextStyle(
             color: Colors.white,
           ),
         ),
-        backgroundColor: Colors.black87,
+        backgroundColor: Colors.grey[900],
         leading: Builder(
           builder: (context) {
             return IconButton(
-              icon: Icon(
+              icon: const Icon(
                 Icons.menu,
                 color: Colors.white,
               ),
@@ -45,22 +43,150 @@ class _HomeScreenState extends State<HomeScreen> {
           ActionsWidget(),
         ],
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: const Center(
-          child: Stack(
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Container for the header with white background
+            Container(
+              color: Colors.white, // White background
+              width: double.infinity,
+              padding: EdgeInsets.all(16),
+              child: Center(
+                child: Text(
+                  '¿Qué servicio deseas hoy?',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
-            ],
-          ),
+            ),
+
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Center(
+                child: Text(
+                  'Resultados',
+                  style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            // Text and Map Section
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Text(
+                    'Peluqueros cerca de tu domicilio. Haz clic abajo para ver el más cercano a ti.',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+
+                  // Image for the map
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Image.asset(
+                          'assets/map-lima.png',
+                          fit: BoxFit.fitWidth,
+                        ),
+                      )
+                    ],
+                  ),
+                  // Address information
+
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Jiron las flores 257 - San Isidro',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(width: 25),
+                      Text(
+                        '¿A donde desea el servicio? ',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+            // Buttons for Reservar and Enviar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.calendar_today,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                      onPressed: () {
+                        // Handle reservation
+                      },
+                    ),
+                    Text(
+                      "Reservar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.local_shipping,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                      onPressed: () {
+                        // Handle reservation
+                      },
+                    ),
+                    Text(
+                      "Enviar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/profile/screens/perfil_screen.dart
+++ lib/profile/screens/perfil_screen.dart
@@ -28,19 +28,20 @@ class _PerfilScreenState extends State<PerfilScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       drawer: SideMenu(),
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Perfil",
           style: TextStyle(
             color: Colors.white,
           ),
         ),
-        backgroundColor: Colors.black87,
+        backgroundColor: Colors.grey[900],
         leading: Builder(
           builder: (context) {
             return IconButton(
-              icon: Icon(
+              icon: const Icon(
                 Icons.menu,
                 color: Colors.white,
               ),
@@ -53,26 +54,95 @@ class _PerfilScreenState extends State<PerfilScreen>
         ],
       ),
       body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            // color: Colors.black87,
-            ),
-        child: const Center(
-          child: Stack(
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [],
-                ),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              // color: Colors.black87,
               ),
-            ],
-          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 5, bottom: 20),
+                    child: Text(
+                      "Perfil",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                ),
+                ItemMenu(
+                  context,
+                  name: "Política de Privacidad",
+                  icono: Icons.lock,
+                  index: null,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ItemMenu(
+                  context,
+                  name: "Recomendar esta app",
+                  icono: Icons.stars,
+                  index: null,
+                ),
+                ItemMenu(
+                  context,
+                  name: "Compartir esta app",
+                  icono: Icons.share,
+                  index: null,
+                ),
+                SizedBox(
+                  height: 285,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 1),
+                  child: Column(
+                    children: [
+                      ItemMenu(
+                        context,
+                        name: "Mensajeria",
+                        icono: Icons.maps_ugc,
+                        index: null,
+                      ),
+                      ItemMenu(
+                        context,
+                        name: "Cerrar sesión",
+                        icono: Icons.power_settings_new,
+                        index: null,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+
+  Widget ItemMenu(BuildContext context,
+      {required String name, required IconData icono, required int? index}) {
+    return ListTile(
+      leading: Icon(
+        icono, // Se elimina 'const' ya que 'icono' es un parámetro
+        color: Colors.white70,
+        size: 35,
+      ),
+      trailing: const Icon(
+        Icons.chevron_right, // Icono de flecha a la derecha
+        color: Colors.white70,
+      ),
+      title: Text(
+        name, // Se elimina 'const' ya que 'name' es un parámetro
+        style: const TextStyle(
+          color: Colors.white,
         ),
       ),
+      onTap: () => {},
     );
   }
 }
